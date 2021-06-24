23f171579bda9623a11faae723a9f1670b537f32
riyorajan <83206714+riyorajan@users.noreply.github.com>
2021-06-24 17:12:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router,Switch, Route} from "react-router-dom";
+import SignUp from './components/pages/Sign Up';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ Contact Us';
+import Home from './components/pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <Router>
+        <Navbar />
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/services" exact component={Services} />
+            <Route path="/products" exact component={Products} />
+            <Route path="/contact-us" exact component={ContactUs} />
+            <Route path="/sign-up" exact component={SignUp} />
+            <Route path="/marketing" exact component={Marketing} />
+            <Route path="/consulting" exact component={Consulting} />
+        </Switch>
+ </Router>
+        
+    );
 }
 
 export default App;
